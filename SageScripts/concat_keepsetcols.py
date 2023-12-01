fb241a4c2bf710c99c18c03e2e2f065aa6537f80
s-babish <sageb498@gmail.com>
2023-11-30 18:55:31 -0800

    Create concat_keepsetcols.py

---
+++ SageScripts/concat_keepsetcols.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+import pandas as pd
+
+concat_df = pd.DataFrame()
+
+numfiles=sys.argv[1]
+fileend=numfiles+2
+for file in sys.argv[2:fileend]:
+    df=pd.read_csv(file)
+    concat_df = pd.concat([concat_df,df.rename(index=str.lower)],ignore_index=True)
+
+out_df=pd.DataFrame()
+for col in sys.argv[fileend:]:
+    out_df = pd.concat([out_df,concat_df[col]],axis=1,ignore_index=True)
+
+out_df.to_csv("concatenatedcols.csv",index=False)
